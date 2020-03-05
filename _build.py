ec6a19ae8e0883904f264c9ab27d4ad35db35207
Bowen3235 <bowen3235@gmail.com>
2020-03-05 12:37:45 +0800

    adding masin

--- _build.py
+++ _build.py
@@ -5,10 +5,31 @@ import os
 import time
 
 def build( self ):
+	self.canvas_list['bg'] = Canvas( self.windows,
+		width=300,
+		height=700,
+		border=0,
+		highlightthickness=0
+	)
+	self.canvas_list['bg'].pack(  )
+
+	## creating option button
 	self.canvas_list['option'] = Canvas( self.windows, 
-		width = 50, 
-		height = 50, 
-		bg='red',
-		 )
-	self.canvas_list['option'].create_rectangle( 0, 0, 50, 50, fill='red' )
-	self.canvas_list['option'].pack( anchor='nw' )
+		width = 50, 		height = 50, 
+		border=0,
+		highlightthickness=0
+	)
+	for i in range(3):
+		self.canvas_list['option'].create_line( 
+			10, 15+i*12, 40, 15+i*12,
+			width  = 6,
+			smooth = True,
+			fill   = 'gray'
+		)	
+	self.canvas_list['option'].place( x=0, y=0, anchor='nw' )
+
+	## creating main canvas
+	self.canvas_list['main_area'] = Canvas( self.windows,
+
+		)
+
